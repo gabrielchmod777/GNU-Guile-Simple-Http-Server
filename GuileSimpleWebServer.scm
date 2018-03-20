f59f9bd97be92e170c118d24f8c6e4b126feae3c
gabriel <gabriel.mcx@gmail.com>
2018-03-20 07:11:10 +0200

    yup ... it works

--- GuileSimpleWebServer.scm
+++ GuileSimpleWebServer.scm
@@ -9,6 +9,7 @@
 (define *mime-types* (make-hash-table 31))
 (hash-set! *mime-types* "css" '("text" . "css"))
 (hash-set! *mime-types* "txt" '("text" . "plain"))
+(hash-set! *mime-types* "html" '("text" . "html"))
 (hash-set! *mime-types* "png" '("image" . "png"))
 (hash-set! *mime-types* "jpg" '("image" . "jpeg"))
 (hash-set! *mime-types* "jpeg" '("image" . "jpeg"))
@@ -34,34 +35,45 @@
 
 
 
+(define (cases request body file-name)
+  (cond ((equal? (request-path-components request) '("duude"))
+	 (values '((content-type . (text/plain))) "Hello duude!"))
+	((file-exists? file-name)
+	 (display "resource .. ok")
+	 (let* ((mime-type (mime-type-ref (uri->string (request-uri request))))
+		(mime-type-symbol (mime-type-symbol mime-type)))
+	   (if (text-mime-type? mime-type)
+	       (values
+		`((content-type . (,mime-type-symbol)))
+		(lambda (out-port)
+		  (call-with-input-file file-name
+		    (lambda (in-port)
+		      (display (read-delimited "" in-port)
+			       out-port)))))
+	       (values
+		`((content-type . (,mime-type-symbol)))
+		(call-with-input-file file-name
+		  (lambda (in-port)
+		    (get-bytevector-all in-port)))))))
+	(else (not-found request)))
+  )
+
+
+(define (no-first-character str)
+  (substring str 1 (string-length str)))
+
 
 (define (hello-hacker-handler request body)
   (display "\n-------------\n")
-  (display (uri->string (request-uri request)))
+  (display (no-first-character (uri->string (request-uri request))))
+  ;;;(display (string? (no-first-character (uri->string (request-uri request)))))
   (display "\n+++++++++++++\n")
 
 
+  (cases request body (no-first-character (uri->string (request-uri request)))  )
 
-  (cond ((equal? (request-path-components request) '("hacker"))
-	 (values '((content-type . (text/html))) "<h2>Hello hacker!</h2> <img src=\"Apple.png\" alt=\"Smiley face\" height=\"42\" width=\"42\"> "))
-	((file-exists? "Apple.png")
-	 (display "EXISTS")
-              (let* ((mime-type (mime-type-ref (uri->string (request-uri request))))
-                     (mime-type-symbol (mime-type-symbol mime-type)))
-                (if (text-mime-type? mime-type)
-                    (values
-                     `((content-type . (,mime-type-symbol)))
-                     (lambda (out-port)
-                       (call-with-input-file "Apple.png"
-                         (lambda (in-port)
-                           (display (read-delimited "" in-port)
-                                                    out-port)))))
-                    (values
-                     `((content-type . (,mime-type-symbol)))
-                     (call-with-input-file "Apple.png"
-                         (lambda (in-port)
-                           (get-bytevector-all in-port)))))))
-	(else (not-found request))))
+
+  )
 
 (define (not-found request)
   (values (build-response #:code 404)
